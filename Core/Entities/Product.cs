dfe0e4a7271c5066b3147efeb0d60252638ac500
Notbook Gabriel <gabrielhma98@gmail.com>
2025-09-01 21:11:10 -0300

    Angular Basics

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -10,18 +10,18 @@ public class Product : BaseEntity
     public required string Size { get; set; }
     public required string Category { get; set; }
     public int QuantityInStock { get; set; }
-    public List<string> ImagesUrl { get; set; } = new(); // Lista de URLs de imagens
+    public List<string> PictureUrl { get; set; } = new(); // Lista de URLs de imagens
 
     // Propriedade para acessar a imagem principal (obrigatória)
     public string ImagemPrincipalUrl
     {
-        get => ImagesUrl.Count > 0 ? ImagesUrl[0] : throw new InvalidOperationException("Produto deve ter pelo menos uma imagem.");
+        get => PictureUrl.Count > 0 ? PictureUrl[0] : throw new InvalidOperationException("Produto deve ter pelo menos uma imagem.");
         set
         {
-            if (ImagesUrl.Count == 0)
-                ImagesUrl.Add(value);
+            if (PictureUrl.Count == 0)
+                PictureUrl.Add(value);
             else
-                ImagesUrl[0] = value;
+                PictureUrl[0] = value;
         }
     }
 }
